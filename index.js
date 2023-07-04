23a43e72a60ae4e9c69c9bd52934e31b1d5d7a67
Gabriel Lopes <123586763+GabrielLDN@users.noreply.github.com>
2023-07-04 14:58:48 -0300

    Commit: conexão com o banco de dados, usando nodemon para atualização continua.
    Atualização do código do repositório da aula 02.
    
    Foi usado o Nodemon para permitir atualização sem derrubar o servidor.
    
    Foi feita a conexão com banco de dados local.

--- index.js
+++ index.js
@@ -5,7 +5,20 @@ const ejs = require("ejs");
 
 const app = express();
 
+const mysql = require("mysql");
 // Configurações
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "banco_node"
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Conectado ao banco de dados");
+});
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -19,7 +32,14 @@ app.get("/", (req, res) => {
 
 app.get("/eventos", (req, res) => {
     
-    res.render("eventos", { eventos: listaDeEventos });
+    const query = "SELECT * FROM eventos";
+    connection.query(query, (err, rows) => {
+        if (err) throw err;
+        
+        res.render("eventos", {rows});
+        
+    }); 
+
     
 });
 
